a110535a1195adcecfb95990af5aa217c58471f9
Stephen Gladney <stephengladney@gmail.com>
2020-09-15 00:34:19 -0400

    convert index.js

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-module.exports.mappings = require("./config/mappings")
-module.exports.Op = require("./config/operators")
+export const mappings = require("./config/mappings")
+export const Op = require("./config/operators")
 
-const { executeAllAutomationRules, Rule } = require("./class/Rule")
-const { rulesWithTrigger, Trigger } = require("./class/Trigger")
+import { executeAllAutomationRules, Rule } from "./class/Rule"
+import { rulesWithTrigger, Trigger } from "./class/Trigger"
 
-module.exports.Rule = Rule
-module.exports.Trigger = Trigger
-module.exports.Condition = require("./class/Condition").Condition
+export const Rule = Rule
+export const Trigger = Trigger
+export const Condition = require("./class/Condition").Condition
 
 let rules
-module.exports.setRules = arr => (rules = arr)
+export const setRules = (arr) => (rules = arr)
+export const addRule = (rule) => rules.push(rule)
 
-module.exports.execute = (trigger, data) => {
+export const execute = (trigger, data) => {
   executeAllAutomationRules(data, rulesWithTrigger(rules, trigger))
 }
