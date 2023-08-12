123603962febb64b8a85450014e27dc40c3d9d72
Stephen Gladney <stephengladney@gmail.com>
2023-08-11 23:25:39 -0400

    refactor index

---
+++ index.ts
@@ -0,0 +1,21 @@
+import { executeAllRules, getAllRulesWithTrigger, rule } from "./functions/rule"
+import { condition } from "./functions/condition"
+import trigger from "./functions/trigger"
+import { addRule, getRules, setLogCallback, rules } from "./functions/crud.js"
+import mappings from "./config/mappings"
+import op from "./config/operators"
+
+const executeAllRulesWithTrigger = (trigger, { data }) =>
+  executeAllRules(getAllRulesWithTrigger(rules, trigger), data)
+
+export default {
+  addRule,
+  condition,
+  executeAllRulesWithTrigger,
+  getRules,
+  mappings,
+  op,
+  rule,
+  setLogCallback,
+  trigger,
+}
