bffb45a3b0ee35fb89b3853f59bf41d4e4624d8e
Stephen Gladney <stephengladney@gmail.com>
2023-08-12 01:05:13 -0400

    rm dist and add to gitignore

--- dist/config/mappings.d.ts
+++
@@ -1,7 +0,0 @@
-export declare const mappings: {
-    Assignee: string;
-    "Card title": string;
-    "Current status": string;
-    "Previous status": string;
-    "Team assigned": string;
-};
--- dist/functions/trigger.d.ts
+++
@@ -1,1 +0,0 @@
-export default function trigger(event: string): string;
